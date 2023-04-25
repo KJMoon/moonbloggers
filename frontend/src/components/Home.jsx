e83f4e16609b4635c38baa036cdddf126af7fba9
Eric Moon <moonkangjin9195@gmail.com>
2023-04-25 14:05:50 -0400

    Add basic frontend ui layout

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 function Home() {
-  return <div>Hello World!</div>;
+  return (
+    <>
+      <Container className="d-none d-sm-block" fluid>
+        <Row>
+          <Col sm className="text-center">
+            Home Page
+          </Col>
+          <Col sm className="text-center">
+            Hello
+          </Col>
+        </Row>
+      </Container>
+      <Container className="d-sm-none" fluid>
+        <Row style={{ height: "400px" }}>
+          <Col sm className="text-center">
+            Home Page
+          </Col>
+          <Col sm className="text-center">
+            Hello
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
 
 export default Home;
