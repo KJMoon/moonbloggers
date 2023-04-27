fcca1a57ce292180dfa862d31be6d391d89a2183
Eric Moon <moonkangjin9195@gmail.com>
2023-04-27 19:13:20 -0400

    Add Profile component and modify Feed component

---
+++ frontend/src/components/Profile.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+import Feed from "./Feed";
+
+function Profile() {
+  const { isAuthenticated, isLoading } = useAuth0();
+  const { username } = useParams();
+
+  const [userPosts, setUserPosts] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/api/profile/${username}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setUserPosts(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  if (isAuthenticated && !isLoading) {
+    return (
+      <>
+        <h2 className="text-center">Profile</h2>
+        <Feed userPosts={userPosts} username={username} />
+      </>
+    );
+  } else {
+    return;
+  }
+}
+
+export default Profile;
