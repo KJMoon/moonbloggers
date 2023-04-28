d452bb94f6ae8f6778ded7db82497f0f63998e1f
Eric Moon <moonkangjin9195@gmail.com>
2023-04-28 13:44:24 -0400

    Set up the routes and controllers for likes

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const app = express();
 const cors = require("cors");
 const mainRoutes = require("./routes/main");
 const postRoutes = require("./routes/posts");
+const likeRoutes = require("./routes/likes");
 
 // Constants
 const PORT = 5000;
@@ -23,6 +24,7 @@ app.get("/", (req, res) => {
 
 app.use("/", mainRoutes);
 app.use("/api/post", postRoutes);
+app.use("/api/likes", likeRoutes);
 
 app.listen(PORT, HOST, () => {
   console.log(`MoonBloggers is running on http://${HOST}:${PORT}`);
